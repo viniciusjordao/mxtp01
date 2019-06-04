89c1388fe74c216c09e2dd85e673bcbe5f96eec3
viniciusjordao <vcs.jordao@gmail.com>
2019-06-04 13:54:21 -0300

    Aplicando o HttpClient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path:'heroes', component: HeroesComponent},
+  { path:'dashboard', component: DashboardComponent},
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path:'dashboard', component: DashboardComponent}
+  { path:'heroes', component: HeroesComponent}
   
 ];
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,9 +10,12 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {  
   
   heroes: Hero[]; 
-  selectedHero: Hero;
+  //selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+
+    ) { }
 
   ngOnInit() {
 
@@ -20,16 +23,27 @@ export class HeroesComponent implements OnInit {
 
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 
-
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
 }
